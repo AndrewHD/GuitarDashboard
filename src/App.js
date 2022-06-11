6e9f4ecac32f3ad4238ecf7b32f3130117474ed7
Andrew Hanson-Dvoracek <andrew.hanson.dvoracek@nehgs.org>
2022-06-11 18:51:10 -0400

    Add random key

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Stopwatch from './components/Stopwatch/Stopwatch.component';
 import Countdown from './components/Countdown/Countdown.component';
 import Metronome from './components/Metronome/Metronome.component';
+import Keys from './components/Keys/Keys.component';
 import ReactModal from 'react-modal';
 
 class App extends Component {
@@ -14,12 +15,17 @@ class App extends Component {
 	handleOpenModal = () => {this.setState({ showModal: true });}
 	handleCloseModal = () => {this.setState({ showModal: false });}
 	
+	componentDidMount() {
+    ReactModal.setAppElement('body');
+ 	}
+ 	
   render() {
 	  return (
 		<div className="App">
 			<div className="CircleFifths">
 				<a title="Just plain Bill, CC BY-SA 3.0 &lt;http://creativecommons.org/licenses/by-sa/3.0/&gt;, via Wikimedia Commons" href="https://commons.wikimedia.org/wiki/File:Circle_of_fifths_deluxe_4.svg"><img width="512" alt="Circle of fifths deluxe 4" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Circle_of_fifths_deluxe_4.svg/512px-Circle_of_fifths_deluxe_4.svg.png"></img></a>
 			</div>
+			<Keys />
 			<button id="showAbout" onClick={this.handleOpenModal}>?</button>
 			<Metronome />
 			<div className="Timers">
